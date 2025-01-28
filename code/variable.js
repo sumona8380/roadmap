f5f69923fd2498b28da3789c715353fd1fad79e4
S8380 <wd.sumona@gmail.com>
2025-01-28 23:45:06 +0600

    variable added

---
+++ code/variable.js
@@ -0,0 +1,7 @@
+var a = 10     // Old style
+let b = 20;    // Preferred for non-const
+const c = 30;  // Preferred for const (cannot be changed)
+
+console.log(a);
+console.log(b);
+console.log(c);
